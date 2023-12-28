73a47d84cc25d0e61dc5e0bc31004f13865a08cf
asuraST <archana@goodworkhub.com>
2023-12-27 21:19:17 -0500

    hotelmanagement add

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static  void main(String[] args){
+        String str = "Please select your preferred choice" + "\n" +
+                "1. Register Account" + "\n" +
+                "2. Book Reservation" + "\n" +
+                "3. Generate Bill" + "\n" +
+                "4. Feedback";
+
+        System.out.println(str);
+    }
+}
