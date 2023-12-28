c8b1c6bd4fa19a31adabb01f185345935f644db5
asuraST <archana@goodworkhub.com>
2023-12-27 21:21:54 -0500

    hotelmanagement add

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
                 "2. Book Reservation" + "\n" +
                 "3. Generate Bill" + "\n" +
                 "4. Feedback";
+
         System.out.println(str);
     }
 }
